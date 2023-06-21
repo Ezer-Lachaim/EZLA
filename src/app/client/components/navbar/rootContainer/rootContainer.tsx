8c29c69d1b1b2aa1022e678551dcd73081b4558c
tomnaor <tomnaor78@gmail.com>
2023-06-21 12:41:30 +0300

    add layout and navbar

---
+++ src/app/client/components/navbar/rootContainer/rootContainer.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export const RootContainer = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-white m-5">{children}</div>;
+};
--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -7,9 +7,5 @@ import Button from '@mui/material/Button';
 export default function Home() {
   const router = useRouter();
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={() => router.push('/client/drives')}>Show drives</Button>
-    </main>
-  );
+  return <Button onClick={() => router.push('/client/drives')}>Show drives</Button>;
 }
