d858ad8c5e3067acc23a9ea2787bae4854f09b4f
sharonamr <a.sharon@gmail.com>
2023-06-21 14:20:24 +0300

    Fixing build

--- src/app/api/drives/route.ts
+++ src/app/api/drives/route.ts
@@ -1,10 +1,5 @@
 import { getAll } from '../repository/user';
 
-export interface Drive {
-  id: number;
-  dest: 'tel aviv';
-}
-
-export async function getDrives(): Promise<Drive[]> {
+export async function getDrives(): Promise<unknown[]> {
   return getAll();
 }
