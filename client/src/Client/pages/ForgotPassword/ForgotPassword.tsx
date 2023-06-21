e4fbe1de79f25ad1a2a146471d35e0720877950f
sharonamr <a.sharon@gmail.com>
2023-06-21 17:55:29 +0300

    Update

--- client/src/Client/pages/ForgotPassword/ForgotPassword.tsx
+++ client/src/Client/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, useEffect, useState} from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -13,6 +13,7 @@ export const ForgotPassword = () => {
 
   useEffect(() => {
     validateEmail();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [email]);
 
   const validateEmail = () => {
@@ -25,28 +26,34 @@ export const ForgotPassword = () => {
       <h1>שכחתם סיסמה ?</h1>
       <p>אנא הזינו את האימייל שלכם ותקבלו קוד אימות באימייל שלכם</p>
       <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { width: '100%' }
-          }}
-          noValidate
-          autoComplete="off"
-          className="w-full"
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { width: '100%' }
+        }}
+        noValidate
+        autoComplete="off"
+        className="w-full"
       >
         <div>
           <TextField
-              className="w-full"
-              id="outlined-password-input"
-              label="אימייל"
-              placeholder="יש להקליד כתובת האימייל"
-              value={email}
-              onChange={handleEmailChange}
-              error={!validEmail}
-              helperText={!validEmail ? 'יש להקליד כתובת אימייל תקינה' : ''}
+            className="w-full"
+            id="outlined-password-input"
+            label="אימייל"
+            placeholder="יש להקליד כתובת האימייל"
+            value={email}
+            onChange={handleEmailChange}
+            error={!validEmail}
+            helperText={!validEmail ? 'יש להקליד כתובת אימייל תקינה' : ''}
           />
         </div>
       </Box>
-      <Button disabled={!validEmail} variant="contained" size="large" className="w-full" type="submit">
+      <Button
+        disabled={!validEmail}
+        variant="contained"
+        size="large"
+        className="w-full"
+        type="submit"
+      >
         שליחה
       </Button>
     </>
--- client/src/Client/pages/ForgotPassword/VerificationCode/VerificationCode.tsx
+++ client/src/Client/pages/ForgotPassword/VerificationCode/VerificationCode.tsx
@@ -6,7 +6,8 @@ export const VerificationCode = () => {
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    email = urlParams.get('email') || 'my_mail@mama.cita';  // remove the default value
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    email = urlParams.get('email') || 'my_mail@mama.cita'; // remove the default value
     if (!email) {
       window.location.href = '/client/forgot-password';
     }
@@ -17,11 +18,8 @@ export const VerificationCode = () => {
       <h1>שלחנו קוד אימות לאימייל שלך</h1>
       <h1>{email}</h1>
       <p>יש להזין את 4 הספרות של קוד האימות</p>
-      <VerificationInput
-          length={4}
-          placeholder=""
-          onChange={(code) => console.log(code)}
-      />
+      {/* eslint-disable-next-line no-console */}
+      <VerificationInput length={4} placeholder="" onChange={(code) => console.log(code)} />
       <p>
         לא קבלתם קוד אימות ? <a href="/forgot-password">לחצו כאן</a>
       </p>
