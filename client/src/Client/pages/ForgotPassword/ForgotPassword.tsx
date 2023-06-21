ff142432cb73c5a1256d43b1f3594026fbe9b821
alex.levin <levinal@gmail.com>
2023-06-21 17:41:26 +0300

    forgot pass screen

--- client/src/Client/pages/ForgotPassword/ForgotPassword.tsx
+++ client/src/Client/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,7 +1,54 @@
+import {ChangeEvent, useEffect, useState} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export const ForgotPassword = () => {
+  const [email, setEmail] = useState('');
+  const [validEmail, setValidEmail] = useState(true);
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target?.value);
+  };
+
+  useEffect(() => {
+    validateEmail();
+  }, [email]);
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setValidEmail(emailRegex.test(email));
+  };
+
   return (
-    <div>
-      <h1>ForgotPassword</h1>
-    </div>
+    <>
+      <h1>שכחתם סיסמה ?</h1>
+      <p>אנא הזינו את האימייל שלכם ותקבלו קוד אימות באימייל שלכם</p>
+      <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { width: '100%' }
+          }}
+          noValidate
+          autoComplete="off"
+          className="w-full"
+      >
+        <div>
+          <TextField
+              className="w-full"
+              id="outlined-password-input"
+              label="אימייל"
+              placeholder="יש להקליד כתובת האימייל"
+              value={email}
+              onChange={handleEmailChange}
+              error={!validEmail}
+              helperText={!validEmail ? 'יש להקליד כתובת אימייל תקינה' : ''}
+          />
+        </div>
+      </Box>
+      <Button disabled={!validEmail} variant="contained" size="large" className="w-full" type="submit">
+        שליחה
+      </Button>
+    </>
   );
 }
