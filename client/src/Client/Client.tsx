e573c62f1ac3e2d5125eb035043f24558f1693a1
Iddan Halevy <iddan.halevy@redis.com>
2023-06-21 16:27:55 +0300

    Add routes

--- client/src/Client/Client.tsx
+++ client/src/Client/Client.tsx
@@ -1,15 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
-import { OrderRide } from './pages/OrderRide/OrderRide.tsx';
-import { Login } from './pages/Login/Login.tsx';
-import { ForgotPasswordRouter } from './pages/ForgotPassword/ForgotPasswordRouter.tsx';
+import { Outlet } from 'react-router-dom';
 
 const Client = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPasswordRouter />} />
-      <Route path="/order-ride" element={<OrderRide />} />
-    </Routes>
+    <main>
+      <Outlet />
+    </main>
   );
 };
 
