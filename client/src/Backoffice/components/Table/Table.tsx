9ccff7996b150bc5a498e37c88c8817e5eafe2cd
sultanyaron <sultanyaron@gmail.com>
2023-06-21 18:48:52 +0300

    Base table

---
+++ client/src/Backoffice/components/Table/Table.tsx
@@ -0,0 +1,59 @@
+import { Table as MuiTable, TableCell, Paper, TableHead, TableRow, TableBody } from '@mui/material';
+import {
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  ColumnDef,
+  flexRender
+} from '@tanstack/react-table';
+
+interface TableProps<TData = object> {
+  data: TData[];
+  columns: ColumnDef<TData, unknown>[];
+}
+
+const Table = ({ data, columns }: TableProps) => {
+  const table = useReactTable({
+    data,
+    columns,
+    // Pipeline
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    //
+    debugTable: true
+  });
+  return (
+    <Paper elevation={2} style={{ padding: '1rem 0px' }}>
+      <MuiTable>
+        <TableHead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableCell key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </MuiTable>
+    </Paper>
+  );
+};
+
+export default Table;
