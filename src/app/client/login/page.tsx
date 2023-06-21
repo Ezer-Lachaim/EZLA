ea62d384627dfd6bcd3d9f29e3b268f112290e4a
Iddan Halevy <iddan.halevy@redis.com>
2023-06-21 14:41:15 +0300

    Add logo

--- src/app/client/login/page.tsx
+++ src/app/client/login/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Metadata } from 'next';
+import Image from 'next/image';
 import { LoginForm } from './form';
 
 export const metadata: Metadata = {
@@ -9,6 +10,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="flex flex-col items-center w-full">
+      <Image src="/logo-large.png" alt="logo" width={106} height={68} />
       <h1 className="text-lg text-blue-600">כניסה למערכת</h1>
       <LoginForm />
       <Link href="/client/forgot-pass" className="mt-5">
