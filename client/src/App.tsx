16acde44c8181f948d23b81b9da9d71038a991c3
Iddan Halevy <iddan.halevy@redis.com>
2023-06-21 16:41:20 +0300

    Login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Backoffice from './Backoffice/Backoffice';
 import Client from './Client/Client';
-import { Login } from './Client/pages/Login/Login.tsx';
 import { ForgotPasswordRouter } from './Client/pages/ForgotPassword/ForgotPasswordRouter.tsx';
 import { OrderRide } from './Client/pages/OrderRide/OrderRide.tsx';
+import Login from './Client/pages/Login/Login.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/client" element={<Client/>}>
+        <Route path="client" element={<Client />}>
           <Route path="login" element={<Login />} />
           <Route path="forgot-password" element={<ForgotPasswordRouter />} />
           <Route path="order-ride" element={<OrderRide />} />
         </Route>
-        <Route path="/backoffice" element={<Backoffice />} />
+        <Route path="backoffice" element={<Backoffice />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/Client/pages/Login/Login.tsx
+++ client/src/Client/pages/Login/Login.tsx
@@ -1,7 +1,92 @@
-export const Login = () => {
+import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import Logo from '../../../assets/logo.png';
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+export default function Login() {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col items-center w-full">
+      <img src={Logo} alt="logo" width={106} height={68} />
+      <h1 className="text-lg text-blue-600">כניסה למערכת</h1>
+      <form className="flex flex-col gap-9 w-full" onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
+          <InputLabel htmlFor="email">אימייל</InputLabel>
+          <OutlinedInput
+            id="email"
+            error={!!errors.email}
+            label="אימייל"
+            {...register('email', { required: true })}
+          />
+          {errors.email?.type === 'required' && (
+            <FormHelperText error className="absolute top-full">
+              חסר אימייל
+            </FormHelperText>
+          )}
+        </FormControl>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="password">סיסמא</InputLabel>
+          <OutlinedInput
+            id="password"
+            error={!!errors.password}
+            type={showPassword ? 'text' : 'password'}
+            label="סיסמא"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPassword((show) => !show)}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            {...register('password', { required: true })}
+          />
+          {errors.password?.type === 'required' && (
+            <FormHelperText error className="absolute top-full">
+              חסר סיסמא
+            </FormHelperText>
+          )}
+        </FormControl>
+
+        <Button variant="contained" size="large" className="w-full" type="submit">
+          כניסה
+        </Button>
+      </form>
+      <Link to="/client/forgot-pass" className="mt-5">
+        שכחתי סיסמא
+      </Link>
+      <div className="absolute bottom-4">
+        <span>אין לך חשבון?</span>
+        &nbsp;
+        <Link to="/client/register">להרשמה</Link>
+      </div>
     </div>
   );
 }
