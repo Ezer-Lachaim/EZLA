4629208e9abb5086a0438de4387ff72a7d5242c5
ETtestim <e.tal@tricentis.com>
2023-06-22 13:47:15 +0300

    fix lint

--- client/src/Backoffice/components/Main/components/modals/RejectCustomerModal/RejectCustomerModal.tsx
+++ client/src/Backoffice/components/Main/components/modals/RejectCustomerModal/RejectCustomerModal.tsx
@@ -1,24 +1,24 @@
-import { Box, Button, Modal, TextField, Typography } from '@mui/material';
-import { useState } from 'react';
-import ClearIcon from '@mui/icons-material/Clear';
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const style = {
-  position: 'absolute' as const,
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as const,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 500,
-  bgcolor: 'background.paper',
-  borderRadius: '4px',
+  bgcolor: "background.paper",
+  borderRadius: "4px",
   boxShadow: 24,
-  p: 2
+  p: 2,
 };
 interface RejectCustomerModalProps {
   open: boolean;
   handleModal: (shouldOpen: boolean) => void;
 }
 function RejectCustomerModal({ open, handleModal }: RejectCustomerModalProps) {
-  const [reason, setReason] = useState('');
+  const [reason, setReason] = useState("");
   return (
     <Modal
       open={open}
@@ -28,12 +28,16 @@ function RejectCustomerModal({ open, handleModal }: RejectCustomerModalProps) {
     >
       <Box sx={style}>
         <div className="flex justify-between">
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          סירוב נוסע חדש
-        </Typography>
-        <Button className="p-0" color="inherit" onClick={() => handleModal(false)}>
-        <ClearIcon />
-        </Button>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            סירוב נוסע חדש
+          </Typography>
+          <Button
+            className="p-0"
+            color="inherit"
+            onClick={() => handleModal(false)}
+          >
+            <ClearIcon />
+          </Button>
         </div>
         <Typography id="modal-modal-description" sx={{ mt: 2, mb: 2 }}>
           יש לכתוב את הסיבה לסירוב
@@ -54,7 +58,7 @@ function RejectCustomerModal({ open, handleModal }: RejectCustomerModalProps) {
             display: "flex",
             justifyContent: "end",
             gap: "10px",
-            marginTop: "15px"
+            marginTop: "15px",
           }}
         >
           <Button
@@ -64,7 +68,11 @@ function RejectCustomerModal({ open, handleModal }: RejectCustomerModalProps) {
           >
             ביטול
           </Button>
-          <Button disabled={!reason} variant="contained" className="bg-red-500 text-white">
+          <Button
+            disabled={!reason}
+            variant="contained"
+            className="bg-red-500 text-white"
+          >
             שליחה
           </Button>
         </Box>
