90886f36862bd0f9becf07b37e9af2e7e46a6312
idan revah <idan.revah@redis.com>
2023-06-22 17:53:58 +0300

    active ride

--- backend/src/routes/rides.ts
+++ backend/src/routes/rides.ts
@@ -4,6 +4,7 @@ import * as controller from '../controllers/rides';
 export const ridesRouter = Router();
 
 ridesRouter.get('/', controller.getAll);
+ridesRouter.get('/active_ride', controller.getActiveRide);
 ridesRouter.get('/:rideId', controller.getRideById);
 ridesRouter.post('/', controller.createRide);
 ridesRouter.put('/:rideId', controller.updateRide);
