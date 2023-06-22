b2de792bf140b4755fb6aa693435b9531d4f6f9b
sultanyaron <sultanyaron@gmail.com>
2023-06-22 08:21:18 +0300

    Adding table pagination

--- client/src/Backoffice/components/Table/Table.tsx
+++ client/src/Backoffice/components/Table/Table.tsx
@@ -1,4 +1,13 @@
-import { Table as MuiTable, TableCell, Paper, TableHead, TableRow, TableBody } from '@mui/material';
+import {
+  Table as MuiTable,
+  TableCell,
+  Paper,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableContainer,
+  TablePagination
+} from '@mui/material';
 import {
   useReactTable,
   getCoreRowModel,
@@ -21,36 +30,54 @@ const Table = <TData extends object>({ data, columns }: TableProps<TData>) => {
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     //
+    initialState: { pagination: { pageSize: 15 } },
     debugTable: true
   });
   return (
     <Paper elevation={2}>
-      <MuiTable>
-        <TableHead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <TableCell key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(header.column.columnDef.header, header.getContext())}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableHead>
-        <TableBody>
-          {table.getRowModel().rows.map((row) => (
-            <TableRow key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </MuiTable>
+      <TableContainer>
+        <MuiTable>
+          <TableHead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableCell key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableHead>
+          <TableBody>
+            {table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </MuiTable>
+        <TablePagination
+          rowsPerPageOptions={[5, 15, 50]}
+          component="div"
+          count={data.length}
+          rowsPerPage={table.getState().pagination.pageSize}
+          page={table.getState().pagination.pageIndex}
+          onPageChange={(_, newPage) => {
+            table.setPageIndex(newPage);
+          }}
+          onRowsPerPageChange={(e) => table.setPageSize(Number(e.target.value))}
+          labelRowsPerPage="שורות בעמוד"
+          labelDisplayedRows={(state) => {
+            return `${state.from}-${state.to} מתוך ${state.count}`;
+          }}
+        />
+      </TableContainer>
     </Paper>
   );
 };
