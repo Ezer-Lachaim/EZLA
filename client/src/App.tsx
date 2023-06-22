6048f826aafd2b3280644625c28ecca1bcdb52cd
sharonamr <a.sharon@gmail.com>
2023-06-22 11:15:53 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import mainRoutes from './Backoffice/Routes/MainRoutes';
 import authRoutes from './Backoffice/Routes/AuthRoutes';
@@ -9,40 +7,25 @@ import ForgotPassword from './Client/pages/ForgotPassword/ForgotPassword';
 import Login from './Client/pages/Login/Login.tsx';
 import OrderRide from './Client/pages/OrderRide/OrderRide.tsx';
 import VerificationCode from './Client/pages/ForgotPassword/VerificationCode/VerificationCode';
-import { Configuration, RideApi, RideStateEnum } from './api-client';
 import Rides from './Client/pages/Rides/Rides.tsx';
-import { BASE_API_URL } from './Config.ts';
-
-const rideapi = new RideApi(new Configuration({ basePath: BASE_API_URL }));
 
 function App() {
-  const [rideState, setRideState] = useState<RideStateEnum>();
-  useEffect(() => {
-    async function fetchData() {
-      const ride = await rideapi.getActiveRideForUser({ userId: 'a' });
-      setRideState(ride.state);
-    }
-    fetchData();
-  }, []);
   return (
-    <>
-      <div>{rideState}</div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="" element={<Client />}>
-            <Route path="login" element={<Login />} />
-            <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="forgot-password/verify" element={<VerificationCode />} />
-            <Route path="register" element={<OrderRide />} />
-            <Route path="rides" element={<Rides />} />
-          </Route>
-          <Route path="/backoffice" element={<Backoffice />}>
-            {mainRoutes.map((route) => route)}
-            {authRoutes.map((route) => route)}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Client />}>
+          <Route path="login" element={<Login />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="forgot-password/verify" element={<VerificationCode />} />
+          <Route path="register" element={<OrderRide />} />
+          <Route path="rides" element={<Rides />} />
+        </Route>
+        <Route path="/backoffice" element={<Backoffice />}>
+          {mainRoutes.map((route) => route)}
+          {authRoutes.map((route) => route)}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
