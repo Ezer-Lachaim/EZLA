ebcbbab638ea3e8eda4bcf3805646e08a01f6ec9
sultanyaron <sultanyaron@gmail.com>
2023-06-22 11:21:52 +0300

    Finish processing user page

--- client/src/Client/pages/ProcessingUserPage/ProcessingUserPage.tsx
+++ client/src/Client/pages/ProcessingUserPage/ProcessingUserPage.tsx
@@ -1,15 +1,20 @@
-import { Box } from '@mui/material';
+import { Typography } from '@mui/material';
 import withLayout from '../../components/LayoutHOC';
 import gears from '../../../assets/gears.png';
 
 const ProcessingUserPage = () => {
   return (
-    <div>
-      <Box>
-        <img src={gears} alt="logo" className="mb-2.5" />
-      </Box>
+    <div className="flex h-full w-full justify-center items-center">
+      <div className="flex flex-col items-center ">
+        <img src={gears} alt="logo" className="w-60" />
+        <h1 className="m-0">פרטי ההרשמה שלך נמצאים בטיפול...</h1>
+        <Typography color="">ניצור איתך קשר כאשר ההרשמה תאושר</Typography>
+      </div>
     </div>
   );
 };
 
-export default withLayout(ProcessingUserPage, { title: 'כניסה לשירות ההסעות' });
+export default withLayout(ProcessingUserPage, {
+  title: 'כניסה לשירות ההסעות',
+  hideBackButton: true
+});
