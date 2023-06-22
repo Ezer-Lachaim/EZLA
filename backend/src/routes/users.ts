9e8f25808a139362994ca5d3bace93a60b6e936f
maor.ohana <maor.ohana@redis.com>
2023-06-22 13:31:46 +0300

    Add me api

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -4,8 +4,8 @@ import * as controller from '../controllers/users';
 export const usersRouter = Router();
 
 usersRouter.get('/', controller.getAll);
+usersRouter.get('/me', controller.get);
+usersRouter.get('/validate_status/:userId', controller.validateStatus);
 usersRouter.post('/', controller.signup);
 usersRouter.post('/login', controller.login);
-usersRouter.post('/:userId', controller.get);
-usersRouter.get('/validate_status/:userId', controller.validateStatus);
 usersRouter.put('/:userId?', controller.updateUser);
