fce75811b8a2e38f8c213f09ce9361812281396b
tomnaor <tomnaor78@gmail.com>
2023-06-22 11:20:35 +0300

    registration form

--- client/src/Client/pages/Register/Register.tsx
+++ client/src/Client/pages/Register/Register.tsx
@@ -1,42 +1,13 @@
-import {
-  Box,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  Step,
-  StepLabel,
-  Stepper,
-  TextField
-} from '@mui/material';
-import { fields } from './fields.ts';
 import withLayout from '../../components/LayoutHOC.tsx';
-
-const steps = ['פרטי הנוסע', 'פרטיים רפואיים', 'סיכום ואישור'];
+import { RegistrationForm } from './components/RegistrationForm/RegistrationForm.tsx';
+import { RegistrationSteps } from './components/RegistrationSteps/RegistrationSteps.tsx';
 
 const Register = () => {
   return (
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={0} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <div className="mt-5 flex flex-col gap-4">
-        {fields.map((textFieldProps) => (
-          <TextField {...textFieldProps} />
-        ))}
-      </div>
-      <div className="mt-5 flex flex-col gap-2">
-        <p className="text-sm text-gray-500">בקשות מיוחדות</p>
-        <FormGroup className="gap-2">
-          <FormControlLabel control={<Checkbox />} label="התאמה לכסא גלגלים" />
-          <FormControlLabel control={<Checkbox />} label="מושב בטיחות לתינוק" />
-          <FormControlLabel control={<Checkbox />} label="מושב בטיחות לילדים (גיל 3-8)" />
-        </FormGroup>
-      </div>
-    </Box>
+    <div className="w-full">
+      <RegistrationSteps activeStepIndex={0} />
+      <RegistrationForm />
+    </div>
   );
 };
 
