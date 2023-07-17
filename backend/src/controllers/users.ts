25e976bae22738d948f9bc2ed3358aebefbca607
maor.ohana <maor.ohana@redis.com>
2023-07-17 16:57:47 +0300

    Fix

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -103,15 +103,19 @@ export const updateUserWithTempToken = async (req: CustomRequest, res: Response)
       res.status(401).send({ error: 'User is not approved!' });
     } else {
       try {
-        const newPassword = req.body.password;
-        await updateUserPassword(req.user.userId, newPassword);
-        await updateIsInitialPass(req.user.userId, false);
-        const userRecord = await firebase.signInWithEmailAndPassword(
-          auth,
-          req.user.email,
-          newPassword
-        );
-        res.status(202).send({ token: await userRecord.user.getIdToken() });
+        if (req.body.user.password) {
+          const newPassword = req.body.user.password;
+          await updateUserPassword(req.user.userId, newPassword);
+          await updateIsInitialPass(req.user.userId, false);
+          const userRecord = await firebase.signInWithEmailAndPassword(
+            auth,
+            req.user.email,
+            newPassword
+          );
+          res.status(202).send({ token: await userRecord.user.getIdToken() });
+        } else {
+          res.status(400).send({ error: 'Missing password' });
+        }
       } catch (e) {
         console.log(e);
         res.status(500).send();
