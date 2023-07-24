e973e77312f623116aaea7e4fb50184a58085d06
Maor Ohana <105290331+maorohana-redis@users.noreply.github.com>
2023-07-24 15:46:37 +0300

    add reset password (#92)

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -9,5 +9,5 @@ usersRouter.get('/validate_status/:userId', controller.validateStatus);
 usersRouter.post('/', controller.signup);
 usersRouter.post('/login', controller.login);
 usersRouter.put('/update_initial_password/:userId?', controller.updateUserWithTempToken);
-usersRouter.get('/reset_password', controller.getResetPasswordLinkForUser);
+usersRouter.post('/reset_password', controller.getResetPasswordLinkForUser);
 usersRouter.put('/:userId?', controller.updateUserFromBO);
