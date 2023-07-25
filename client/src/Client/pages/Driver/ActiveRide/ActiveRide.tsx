5c020458e62ed1ae5895c17183954ecac196000d
Iddan Halevy <iddan.halevy@redis.com>
2023-07-25 23:02:01 +0300

    active ride screen (#114)
    Co-authored-by: alex.levin <levinal@gmail.com>

--- client/src/Client/pages/Driver/ActiveRide/ActiveRide.tsx
+++ client/src/Client/pages/Driver/ActiveRide/ActiveRide.tsx
@@ -1,29 +1,73 @@
 import { useEffect, useState } from 'react';
+import DirectionsCarFilledIcon from '@mui/icons-material/DirectionsCarFilled';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import withLayout from '../../../components/LayoutHOC.tsx';
 import { Ride, RideStateEnum } from '../../../../api-client';
 import { api } from '../../../../Config.ts';
+import ConfirmCancelRideModal from '../../../components/ConfirmCancelRideModal/ConfirmCancelRideModal.tsx';
 
 const ActiveRide = () => {
+  const [confirmClose, setConfirmClose] = useState(false);
   const [ride, setRide] = useState<Ride>();
+  const navigate = useNavigate();
+
   useEffect(() => {
     (async () => {
       setRide(await api.ride.getActiveRideForUser());
     })();
   }, []);
 
-  const finishRide = async () => {
+  const onArrive = async () => {
     await api.ride.updateRide({
       rideId: ride?.rideId || '',
-      ride: { ...ride, state: RideStateEnum.Completed }
+      ride: { ...ride, state: RideStateEnum.DriverArrived }
     });
-    console.log('Ride completed');
+    console.log('Arrived');
+  };
+
+  const onCancel = async () => {
+    await api.ride.updateRide({
+      rideId: ride?.rideId || '',
+      ride: { ...ride, state: RideStateEnum.DriverCanceled }
+    });
+    console.log('Ride canceled');
+
+    navigate('/driver/rides');
   };
 
   return (
-    <div className="w-full h-full absolute top-0 bg-gray-200 overflow-auto">
-      {JSON.stringify(ride)}
-      <Button onClick={finishRide}>Complete ride</Button>
+    <div className="w-full overflow-auto">
+      <div className="bg-yellow-300 text-center rounded-md">
+        <DirectionsCarFilledIcon className="fill-orange-600" />
+        <p>נוסעים לאסוף את הנוסעים </p>
+      </div>
+      <p className="text-center mt-4">{`זמן הגעה משוער ${ride?.destinationArrivalTime}`}</p>
+      <div>
+        <p className="mt-4">שם הנוסע</p>
+        <p>{`${ride?.rideRequester?.firstName} ${ride?.rideRequester?.lastName}`}</p>
+        <p>כתובת איסוף</p>
+        <p>{ride?.origin}</p>
+        <p>כתובת יעד</p>
+        <p>{ride?.destination}</p>
+        <p>בקשות מיוחדות</p>
+        {ride?.specialRequest?.map((specialRequest) => (
+          <p>{specialRequest}</p>
+        ))}
+      </div>
+
+      <Button variant="contained" onClick={onArrive}>
+        הגעתי למקום האיסוף
+      </Button>
+      <Button variant="outlined" color="error" onClick={() => setConfirmClose(true)}>
+        ביטול הנסיעה
+      </Button>
+
+      <ConfirmCancelRideModal
+        open={confirmClose}
+        onCancel={onCancel}
+        onContinue={() => setConfirmClose(false)}
+      />
     </div>
   );
 };
--- client/src/Client/pages/Passenger/ActiveRide/ActiveRide.tsx
+++ client/src/Client/pages/Passenger/ActiveRide/ActiveRide.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import withLayout from '../../../components/LayoutHOC.tsx';
-import { Ride } from '../../../../api-client';
+import { Ride, RideStateEnum } from '../../../../api-client';
 import { api } from '../../../../Config.ts';
+import DriverCanceledModal from './DriverCanceledModal.tsx';
 
 const ActiveRide = () => {
+  const navigate = useNavigate();
   const [ride, setRide] = useState<Ride>();
   useEffect(() => {
     (async () => {
@@ -11,9 +14,45 @@ const ActiveRide = () => {
     })();
   }, []);
 
+  const canceledRide = async () => {
+    await api.ride.updateRide({
+      rideId: ride?.rideId || '',
+      ride: { ...ride, state: RideStateEnum.Canceled }
+    });
+  };
+
+  const onConfirmCancelRide = async () => {
+    await canceledRide();
+    navigate('/passenger/order-ride');
+  };
+
+  const onOrderNewRide = async () => {
+    await canceledRide();
+
+    const response = await api.ride.ridesPost({
+      ride: {
+        ...ride,
+        state: RideStateEnum.WaitingForDriver,
+        rideId: undefined,
+        requestTimeStamp: undefined,
+        driver: undefined
+      }
+    });
+
+    console.log(response);
+
+    navigate('/passenger/order-ride');
+  };
+
   return (
     <div className="w-full h-full absolute top-0 bg-gray-200 overflow-auto">
       {JSON.stringify(ride)}
+
+      <DriverCanceledModal
+        open={ride?.state === RideStateEnum.DriverCanceled}
+        onNewRide={onOrderNewRide}
+        onCancel={onConfirmCancelRide}
+      />
     </div>
   );
 };
