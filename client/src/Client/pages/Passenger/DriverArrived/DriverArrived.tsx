d7a9ce67e9e8b41d359269cfba11250aff369d7f
tomnaor <tomnaor78@gmail.com>
2023-08-07 17:34:27 +0300

    driver arrived

--- client/src/Client/pages/Passenger/DriverArrived/DriverArrived.tsx
+++ client/src/Client/pages/Passenger/DriverArrived/DriverArrived.tsx
@@ -1,32 +1,49 @@
 import { Phone, CheckCircle } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import withLayout from '../../../components/LayoutHOC.tsx';
 import { useUserContext } from '../../../../context/UserContext/UserContext.tsx';
+import { ViewField } from '../../../components/ViewField/ViewField.tsx';
+import { SpecialRequestsChips } from '../../../components/SpecicalRequests/SpecialRequests.tsx';
 
 const DriverArrived = () => {
   const { activeRide: ride } = useUserContext();
 
   return (
-    <div className="w-full overflow-auto">
-      <div className="bg-green-300 text-center rounded-md">
-        <CheckCircle className="fill-green-600" />
-        <p>ההסעה הגיע!</p>
-      </div>
-      <div>
+    <div className="w-full pb-5 h-full flex flex-col">
+      <Box
+        sx={{ background: '#4CAF501A' }}
+        className="rounded-md flex items-center flex-col py-4 gap-2"
+      >
+        <CheckCircle sx={{ fill: '#4CAF50' }} fontSize="large" />
+        <h1 className="font-medium m-0" style={{ color: '#4CAF50' }}>
+          ההסעה הגיעה!
+        </h1>
+      </Box>
+      <h1 className="text-center">המתנדב ממתין לכם</h1>
+      <div className="flex-1">
         <hr />
-        <b className="mt-4">שם המתנדב</b>
-        <p>{`${ride?.driver?.firstName} ${ride?.driver?.lastName}`}</p>
-        <b>סוג רכב</b>
-        <p>
-          {ride?.driver?.carManufacturer} {ride?.driver?.carModel}
-        </p>
-        <b>לוחית זיהוי</b>
-        <p>{ride?.driver?.carPlateNumber}</p>
-        <hr />
-        <b>כתובת איסוף</b>
-        <p>{ride?.origin}</p>
-        <b>כתובת יעד</b>
-        <p>{ride?.destination}</p>
+        <ViewField
+          label="שם המתנדב"
+          value={`${ride?.driver?.firstName} ${ride?.driver?.lastName}`}
+        />
+        <ViewField
+          label="סוג רכב"
+          value={
+            ride?.driver?.carManufacturer
+              ? `${ride?.driver?.carManufacturer} ${ride?.driver?.carModel}`
+              : ''
+          }
+        />
+        <ViewField label="לוחית זיהוי" value={ride?.driver?.carPlateNumber || ''} />
+
+        <hr className="mt-2" />
+        <ViewField label="כתובת איסוף" value={ride?.origin || ''} />
+        <ViewField label="כתובת יעד" value={ride?.destination || ''} />
+
+        <div className="mt-2">
+          <p className=" text-sm text-gray-500">בקשות מיוחדות</p>
+          <SpecialRequestsChips specialRequests={ride?.specialRequest || []} />
+        </div>
       </div>
 
       <Button variant="outlined" href={`tel:${ride?.driver?.cellPhone}`}>
