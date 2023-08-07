b464618b9bef3b1182c1471c4e4a0294c940d48f
Iddan Halevy <iddan.halevy@redis.com>
2023-08-07 19:15:52 +0300

    Register fixes

--- client/src/Client/pages/Register/hooks/useRegistrationSteps.ts
+++ client/src/Client/pages/Register/hooks/useRegistrationSteps.ts
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 
 export const useRegistrationSteps = () => {
-  const [activeStepIndex, setActiveStepIndex] = useState<number>(0);
+  const [activeStepIndex, setActiveStepIndex] = useState<number>(
+    parseInt(sessionStorage.getItem('activeStepIndex') || '0', 10)
+  );
 
   const nextStep = () => {
     setActiveStepIndex(activeStepIndex + 1);
+    sessionStorage.setItem('activeStepIndex', (activeStepIndex + 1).toString());
   };
 
   const previousStep = () => {
     setActiveStepIndex(activeStepIndex - 1);
+    sessionStorage.setItem('activeStepIndex', (activeStepIndex - 1).toString());
   };
 
   return {
