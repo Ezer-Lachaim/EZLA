c71a66b8d8cad0a753a2cc4a69c0a9376c80c1d1
slavaPrus <slava.ei@gmail.com>
2023-10-10 23:55:27 +0300

    Order Ride: Enlarge number of passengers to 12

--- client/src/Client/pages/Passenger/OrderRide/OrderRide.tsx
+++ client/src/Client/pages/Passenger/OrderRide/OrderRide.tsx
@@ -257,6 +257,13 @@ const OrderRide = () => {
             <MenuItem value={3}>3</MenuItem>
             <MenuItem value={4}>4</MenuItem>
             <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={6}>6</MenuItem>
+            <MenuItem value={7}>7</MenuItem>
+            <MenuItem value={8}>8</MenuItem>
+            <MenuItem value={9}>9</MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={11}>11</MenuItem>
+            <MenuItem value={12}>12</MenuItem>
           </Select>
           {errors.passengerCount?.type === 'required' && (
             <FormHelperText error className="absolute top-full mr-0">
