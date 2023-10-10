59e34785b4ff007e869572cf8618be9d157f8687
Leibale <me@leibale.com>
2023-10-10 18:27:04 -0400

    use capacitor to build an android app

---
+++ client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+export default {
+  appId: 'com.EZLA',
+  appName: 'עזר לחיים',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+} satisfies CapacitorConfig;
