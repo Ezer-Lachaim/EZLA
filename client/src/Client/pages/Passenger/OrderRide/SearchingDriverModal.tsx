bfa22aea3371652bf888aab43abd802b589f0d8d
Iddan <iddan.halevy@redis.com>
2023-10-10 19:09:10 +0300

    Implementing #214

--- client/src/Client/pages/Passenger/OrderRide/SearchingDriverModal.tsx
+++ client/src/Client/pages/Passenger/OrderRide/SearchingDriverModal.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Modal, Box, Button, IconButton } from '@mui/material';
-import { Cancel, Close } from '@mui/icons-material';
+import { Box, Button } from '@mui/material';
+import { Cancel } from '@mui/icons-material';
 import car from '../../../../assets/car.png';
 import ConfirmCancelRideModal from '../../../components/ConfirmCancelRideModal/ConfirmCancelRideModal.tsx';
 
 const style = {
-  position: 'absolute' as const,
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 320,
-  height: 300,
+  position: 'fixed' as const,
+  top: '60px',
+  left: '0',
+  width: '100vw',
+  height: 'calc(100vh - 60px)',
   bgcolor: 'background.paper',
-  borderRadius: '4px',
-  boxShadow: 24,
-  p: 2.5
+  p: 2.5,
+  zIndex: '10'
 };
 
 const SearchingDriverModal = ({
@@ -27,13 +25,18 @@ const SearchingDriverModal = ({
   const [confirmClose, setConfirmClose] = React.useState(false);
 
   return (
-    <Modal open={open} disablePortal disableEscapeKeyDown>
+    // <Modal open={open} disablePortal disableEscapeKeyDown>
+    open && (
       <>
         <Box sx={style}>
           <div className="flex flex-col w-full h-full">
             <div className="flex flex-col justify-center flex-grow items-center">
               <img src={car} alt="car" className="w-20 animate-bounce" />
-              <h1 className="text-center">מחפשים עבורכם מתנדב...</h1>
+              <h1 className="text-center">פנייתכם נקלטה</h1>
+              <h1 className="text-center">אנא העזרו בסבלנות, זה עשוי לקחת מספר דקות</h1>
+              <h1 className="text-center">
+                במידה ואין מתנדבים זמינים כרגע, אנחנו ניצור אתכם קשר לפני שיצאו לדרך
+              </h1>
             </div>
             <Button
               variant="outlined"
@@ -45,13 +48,13 @@ const SearchingDriverModal = ({
               ביטול נסיעה
             </Button>
           </div>
-          <IconButton
-            size="small"
-            className="absolute left-2 top-1"
-            onClick={() => setConfirmClose(true)}
-          >
-            <Close />
-          </IconButton>
+          {/* <IconButton */}
+          {/*  size="small" */}
+          {/*  className="absolute left-2 top-1" */}
+          {/*  onClick={() => setConfirmClose(true)} */}
+          {/* > */}
+          {/*  <Close /> */}
+          {/* </IconButton> */}
         </Box>
 
         <ConfirmCancelRideModal
@@ -63,7 +66,8 @@ const SearchingDriverModal = ({
           onContinue={() => setConfirmClose(false)}
         />
       </>
-    </Modal>
+    )
+    // </Modal>
   );
 };
 
