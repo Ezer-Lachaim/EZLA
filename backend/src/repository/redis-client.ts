b5147142191820cdd522eed6936471e0e6bb100f
maor.ohana <maor.ohana@redis.com>
2023-11-08 10:08:47 +0200

    read ca cert from env var

--- backend/src/repository/redis-client.ts
+++ backend/src/repository/redis-client.ts
@@ -1,15 +1,14 @@
 import { createClient } from 'redis';
 import dotenv from 'dotenv';
-import { readFileSync } from 'fs';
 
 dotenv.config();
-
+console.log(process.env.REDIS_CA_CERT);
 const client = createClient({
   socket: {
     host: process.env.REDIS_HOST || 'localhost',
     port: parseInt(process.env.REDIS_PORT || '6379', 10),
-    tls: process.env.REDIS_TLS_ENABLED === 'true',
-    cert: readFileSync('redis-ca.crt')
+    tls: !!process.env.REDIS_CA_CERT,
+    cert: (process.env.REDIS_CA_CERT || '').replace(/\\n/g, '\n')
   },
   username: process.env.REDIS_USER || '',
   password: process.env.REDIS_PASS || ''
