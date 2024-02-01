71cf0e2a4590694fe334efe05425b7fb9b192b88
Yanki Baras <y8463834@gmail.com>
2024-02-01 16:58:06 +0200

    eodify the header names

--- client/src/Backoffice/components/Main/components/Rides/Rides.tsx
+++ client/src/Backoffice/components/Main/components/Rides/Rides.tsx
@@ -53,6 +53,7 @@ const columns: ColumnDef<Partial<Ride>>[] = [
   {
     accessorKey: 'cellphone',
     header: 'טלפון ליצירת קשר',
+<<<<<<< HEAD
     cell: ({ row }) => {
       const { cellphone } = row.original;
       return cellphone ? (
@@ -68,6 +69,9 @@ const columns: ColumnDef<Partial<Ride>>[] = [
         '-'
       );
     }
+=======
+    accessorFn: (data) => data.cellphone || '-'
+>>>>>>> 57a4c05 (modify the header names)
   },
   {
     accessorKey: 'origin',
