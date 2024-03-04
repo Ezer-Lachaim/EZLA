fbaa0a9243f1a29bfcd16f013287991bda2c7fae
YankiBaras <y8463834@gmail.com>
2024-03-04 13:02:30 +0200

    first styling of the settings

--- client/src/Backoffice/components/Main/components/Settings/Settings.tsx
+++ client/src/Backoffice/components/Main/components/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import { Switch, TextField, Typography } from '@mui/material';
+import { Box, Switch, TextField, Typography } from '@mui/material';
 import { ChangeEvent, useState } from 'react';
 
 const Settings = () => {
@@ -15,37 +15,41 @@ const Settings = () => {
   };
 
   return (
-    <div className='mt-20 gap-5'>
-      <div>
-        <Typography className="text-lg text-blue-500">
-        <Switch checked={isRoundTripEnabled} onChange={handleRoundTripToggle} />
-
-          הפעל נסיעה הלוך וחזור
+    <div className="mt-20 gap-5 bg-white h-full p-5">
+        <Box sx={{fontWeight: '500', fontSize: '22px', color: '#007DFF'}}>
+           הגדרות 
+        </Box>
+      <div className="flex flex-row mt-10">
+        <Typography className=" text-lg font-normal ml-10  opacity-80">
+          מינימום התראה להזמנת נסיעה (שעות) <br />
+          <p className=" text-base opacity-60">נוסעים לא יוכלו להזמין נסיעה במועד מוקדם מהמינימום זמן שנקבע</p>
         </Typography>
-      </div>
-      <div className='flex flex-col mt-10'>
-        <Typography className=' text-lg font-semibold text-blue-500'>
-          מגבלת שעות שרק אחריהן ניתן להזמין:
-          </Typography>
 
-          <TextField
-          className='mt-5 w-40'
-                id="hours"
-                style={{backgroundColor: 'white'}}
-                required
-                label="מספר שעות"
-                value={inviteTimeLimit}
-                onChange={handleInviteTimeChange}
-                type="number"
-                inputProps={{ min: 0, inputMode: 'numeric' }}
-                defaultValue={1}
-                sx={{
-                  '& input[type="number"]::-webkit-inner-spin-button, & input[type="number"]::-webkit-outer-spin-button':
-                    {
-                      opacity: 1
-                    }
-                }}
-              />
+        <TextField
+          className="mt-5 w-40"
+          id="hours"
+          style={{ backgroundColor: 'white' }}
+          required
+          label="מינימום התראה בשעות"
+          value={inviteTimeLimit}
+          onChange={handleInviteTimeChange}
+          type="number"
+          inputProps={{ min: 0, inputMode: 'numeric' }}
+          defaultValue={1}
+          sx={{
+            '& input[type="number"]::-webkit-inner-spin-button, & input[type="number"]::-webkit-outer-spin-button':
+              {
+                opacity: 1
+              }
+          }}
+        />
+      </div>
+      <div>
+        <Typography className="text-base  opacity-80">
+          <Switch checked={isRoundTripEnabled} onChange={handleRoundTripToggle} />
+          בקשה לנסיעה הלוך ושוב
+          <p className="opacity-60">הנוסע יוכל לבקש נסיעה הלוך ושוב. זוהי אינדיקציה בלבד. לא יווצרו שתי נסיעות.</p>
+        </Typography>
       </div>
     </div>
   );
